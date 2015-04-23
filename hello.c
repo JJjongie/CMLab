0f164c5592c7bb58d4771cdfa08e90eda6c4ea9a
JJjongie <n.chungsawat@gmail.com>
2015-04-23 17:18:51 +0700

    add and commit new file by User_A

--- hello.c
+++ hello.c
@@ -2,8 +2,8 @@
 int main()
 {
   char act[25];
-  printf("Hello world\n");
-  printf("EiEi Gum\n");
+  printf("Good morning\n"); //Edit by User A
+  printf("Have a good days\n");//Edit by User A
   printf("What are you doing\n");
   scanf("%s",act);
   return 0;
